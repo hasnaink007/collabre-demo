08012cad88a921406d3b34bb25a119d2db918dc5
Hasnain Khalid <ch.hasnainkhalid@gmail.com>
2020-09-27 02:56:23 +0500

    csp update to concat imgSrcs

--- server/csp.js
+++ server/csp.js
@@ -92,15 +92,13 @@ module.exports = (reportUri, enforceSsl, reportOnly) => {
   // https://content-security-policy.com/
 
   // Example: extend default img directive with custom domain
-  // const { imgSrc = [self] } = defaultDirectives;
-  // const exampleImgSrc = imgSrc.concat('my-custom-domain.example.com');
+  const { imgSrc = [self] } = defaultDirectives;
+  const updatedImgSrc = imgSrc.concat('*.amazonaws.com');
 
   const customDirectives = {
     // Example: Add custom directive override
     // imgSrc: exampleImgSrc,
-    imgSrc:[
-      '*.amazonaws.com'
-    ]
+    imgSrc:updatedImgSrc,
   };
 
   // ================ END CUSTOM CSP URLs ================ //
