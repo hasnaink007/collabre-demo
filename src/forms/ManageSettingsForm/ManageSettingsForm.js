ecfc9267d8d8d20ef354b197b967a4b706c51dc5
Ejaz_Ahmad <ejazahmad4772@gmail.com>
2020-10-13 14:05:30 -0400

    settings form

--- src/forms/ManageSettingsForm/ManageSettingsForm.js
+++ src/forms/ManageSettingsForm/ManageSettingsForm.js
@@ -14,31 +14,132 @@ class ManageSettingsForm extends Component {
         this.state = {
             sections:[
                 {
-                    name:'section_1'
+                    name:'section_1',
+                    desc: 'descriptipn_1',
+                    items: []
                 },
                 {
-                    name:'section_2'
+                    name:'section_2',
+                    desc: 'descriptipn_1',
+                    items: [
+                        {
+                            label: 'something',
+                            url: 'someURL',
+                            image: 'someImage'
+                        }
+                    ]
                 },
                 {
-                    name:'section_3'
+                    name:'section_3',
+                    desc: 'descriptipn_1',
+                    items: [
+                        {
+                            label: 'something',
+                            url: 'someURL',
+                            image: 'someImage'
+                        },
+                        {
+                            label: 'something',
+                            url: 'someURL',
+                            image: 'someImage'
+                        }
+                    ]
                 }
             ]
         }
     }
 
-    sectionSet = (section) => {
-        console.log(section);
-        return (
-            <>
-            <input type="text" name={section.name} placeHolder="Section Title"></input>
-            
-            </>
-        )
+    // sections: [
+    //     {
+    //         title:'',
+    //         description:'',
+    //         items: [
+    //             {
+    //                 label:'',
+    //                 url:'',
+    //                 image:'',
+    //             }
+    //         ]
+    //     }
+    // ]
 
+    sectionSet = (section, section_index) => {
+        // console.log(section);
+        let items_heading = ''
+        if( section.items.length ){
+            items_heading = <h4>Items</h4>
+        }
+        //==============
+
+        return (
+            <div>
+                <h3>Section</h3>
+                <input type="text" name={section.name} placeholder="Section Title" />
+                <input type="text" name={section.desc} placeholder="Section Description"/>
+                {items_heading}
+                {section.items.map((item,index) => { return this.item(item, index, section_index) })}
+                <button type="button" onClick={event => this.addItem(event,section_index)} className={css.addItemBtn}> Add Item </button>
+            
+            </div>
+        )
 
     }
 
 
+//=================
+
+    addItem = (event, index) => {
+        // console.log(event)
+        
+        const oldStateSections = this.state.sections
+        
+        oldStateSections[index].items.push({
+            label:'Label', image:'Image', url:'URL' 
+        })
+
+        this.setState({...this.state, sections: oldStateSections})
+    }
+
+//=================
+
+    item = (item, item_index, section_index) => {
+        // console.log(item,index,section)
+        // console.log('=========================================')
+        return(
+            <div className={css.itemsContainer}>
+                <span className={css.closeBtn} onClick={event => this.removeItem(item_index, section_index)}>x</span>
+                <div className={css.items}>
+                    <input type="text" name={'label_'+ item_index} placeholder={item.label} />
+                    <input type="text" name={'url_'+ item_index} placeholder={item.url} />
+                    <input type="text" name={'image_' + item_index} placeholder={item.image} />
+                </div>
+            </div>
+        )
+    }
+
+//=================
+
+    removeItem = (item_index, section_index) =>{
+        // console.log(item_index)
+        // console.log(section_index)
+        let newSections = this.state.sections
+        delete newSections[section_index].items[item_index]
+
+        // let oldStateSections = this.state.sections
+
+        // let sectionIndex = oldStateSections.findIndex(csection => { return csection == section } )
+
+        // console.log(sectionIndex)
+        // console.log('========================')
+        // delete oldStateSections[sectionIndex][item_index]
+
+        this.setState({
+            ...this.state, sections: newSections
+        })
+
+    }
+
+//=================
 
     submitForm = (values) => {
         console.log('Form Submitted');
@@ -48,7 +149,9 @@ class ManageSettingsForm extends Component {
         let newArray = this.state.sections;
         newArray.push(
             {
-                name:`section_${this.state.sections.length + 1}`
+                name: 'Section Title',
+                desc: 'Section Description',
+                items: []
             }
         );
         this.setState({
@@ -70,7 +173,7 @@ class ManageSettingsForm extends Component {
                     {this.state.sections.map(this.sectionSet)}
                 
                 
-                    <button onClick={this.addSection}>Add Section</button>
+                    <button type="button" onClick={this.addSection}>Add Section</button>
                 </div>    
                 
                     
